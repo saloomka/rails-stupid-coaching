e48dd905e105fd89a29ca119104d2bc1ffb15ec3
Salome Abramishvili <salomeeabramishvili@gmail.com>
2022-11-14 15:28:25 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  
+  def home
+
+  end
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+    if params[:question].include?("?")
+      @answer = "Silly question, get dressed and go to work!."
+    elsif params[:question] == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "questions#home"
+
+  get "about", to: "questions#about"
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#answer"
+  get "contact", to: "questions#contact"
 
   # Defines the root path route ("/")
   # root "articles#index"
